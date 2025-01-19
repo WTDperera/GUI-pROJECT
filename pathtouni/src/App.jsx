d2b8f7be0896e471a99fe47cc066f4f295e1a14d
Tharindu Perera <www.tharindudinindaperera@gmail.com>
2025-01-20 01:39:17 +0530

    created basic ui designs

---
+++ pathtouni/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Knight from './Components/Knight/Knight'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Knight/>
+    </div>
+  )
+}
+
+export default App
